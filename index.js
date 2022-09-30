9ed6040938f3091c5a550331315b95f689e6ef6a
Yuri3358 <yurialmeida917@gmail.com>
2022-09-30 18:54:59 -0300

    Size modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function Generate() {
     const list_candidates = []
-    const list_roles = []
     const entries = document.querySelectorAll(".candinput")
     const roles = document.querySelectorAll(".numboxers")
     entries.forEach((number) => list_candidates.push(number.value))
