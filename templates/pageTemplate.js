f26c80f3f29df31a389f2e4de4a5b98470db271d
Yuri3358 <yurialmeida917@gmail.com>
2023-04-06 19:44:55 -0300

    feat(.candinput): maxlength assigned to elements

--- templates/pageTemplate.js
+++ templates/pageTemplate.js
@@ -2,19 +2,19 @@ const pageTemplate = `
 <header>Cola Eleitoral {{ year }}</header>
 <div id="candidates-form">
     <label for="president">Pres. da República</label>
-    <input class="candinput" id="president">
+    <input class="candinput" id="president" maxlength="2">
 
     <label for="senator">Senador da Rep.</label>
-    <input class="candinput" id="senator">    
+    <input class="candinput" id="senator" maxlength="3">    
 
     <label for="depfed">Deputado Federal</label>
-    <input class="candinput" id="depfed">
+    <input class="candinput" id="depfed" maxlength="4">
     
     <label for="gov">Governador</label>
-    <input class="candinput" id="gov">
+    <input class="candinput" id="gov" maxlength="2">
     
     <label for="depest">Deputado Estadual</label>
-    <input class="candinput" id="depest">
+    <input class="candinput" id="depest" maxlength="5">
 </div>
 
 <div id="candidates-display">
