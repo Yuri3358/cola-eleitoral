7f96ed17f1b715ef8f324a4cdf0ee15056ed4394
Yuri3358 <yurialmeida917@gmail.com>
2023-04-06 20:10:58 -0300

    feat: v-models inputs

--- templates/pageTemplate.js
+++ templates/pageTemplate.js
@@ -1,24 +1,35 @@
 const pageTemplate = `
-<header>Cola Eleitoral {{ year }}</header>
-<div id="candidates-form">
+<header id="app-header">
+    <h2>
+        Cola Eleitoral {{ year }} {{ color }}
+    </h2>
+    <h3 id="watermark">Made by 
+        <a target="_blank" href="https://github.com/Yuri3358">Yuri3358</a>
+    </h3>
+</header>
+<div class="box" id="candidates-form">
     <label for="president">Pres. da República</label>
-    <input class="candinput" id="president" maxlength="2">
+    <input class="candinput" id="president" maxlength="2" v-model="candidates.president">
 
     <label for="senator">Senador da Rep.</label>
-    <input class="candinput" id="senator" maxlength="3">    
+    <input class="candinput" id="senator" maxlength="3" v-model="candidates.senator">    
 
     <label for="depfed">Deputado Federal</label>
-    <input class="candinput" id="depfed" maxlength="4">
+    <input class="candinput" id="depfed" maxlength="4" v-model="candidates.federal">
     
     <label for="gov">Governador</label>
-    <input class="candinput" id="gov" maxlength="2">
+    <input class="candinput" id="gov" maxlength="2" v-model="candidates.governor">
     
     <label for="depest">Deputado Estadual</label>
-    <input class="candinput" id="depest" maxlength="5">
+    <input class="candinput" id="depest" maxlength="5" v-model="candidates.state">
 </div>
 
 <div id="candidates-display">
-    <p>outro teste!</p>
+    <p>{{ candidates.president }}</p>
+    <p>{{ candidates.senator }}</p>
+    <p>{{ candidates.federal }}</p>
+    <p>{{ candidates.governor }}</p>
+    <p>{{ candidates.state }}</p>
 </div>
 `
 
