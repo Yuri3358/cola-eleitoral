cd40e92eb051cfb54094124d356eecbfb72388e6
Yuri3358 <yurialmeida917@gmail.com>
2023-04-06 18:28:55 -0300

    BREAKING CHANGE: all files deleted/cleaned

--- script.js
+++
@@ -1,7 +0,0 @@
-function Generate() {
-    const list_candidates = []
-    const entries = document.querySelectorAll(".candinput")
-    const roles = document.querySelectorAll(".numboxers")
-    entries.forEach((number) => list_candidates.push(number.value))
-    roles.forEach((names, index) => names.innerHTML = list_candidates[index])
-}
